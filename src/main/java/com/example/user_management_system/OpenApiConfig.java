1ed87e555366de46a3dc70786257a3ddb340c050
shankar <coder.shankar@gmail.com>
2024-06-10 11:30:23 +0545

    Fix swagger ui after auth added

--- src/main/java/com/example/user_management_system/OpenApiConfig.java
+++ src/main/java/com/example/user_management_system/OpenApiConfig.java
@@ -1,11 +1,20 @@
 package com.example.user_management_system;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenApiConfig {
 
     @Bean
